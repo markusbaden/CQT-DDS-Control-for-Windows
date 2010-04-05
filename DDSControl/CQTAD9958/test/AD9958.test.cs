e2ea2fd9dcb4e45b9290f1528bdc142b09037792
Markus <markus@localhost>
2010-04-05 06:10:04 +0000

    Rename private messages in test (reomoved trailing msg(

--- DDSControl/CQTAD9958/test/AD9958.test.cs
+++ DDSControl/CQTAD9958/test/AD9958.test.cs
@@ -14,10 +14,10 @@ namespace DDSControl
         private Mockery mocks;
         private IDDSMicrocontroller mockDevice;
 
-        private Message setTwoLevelMsg;
-        private Message setSingleToneMsg;
-        private Message selectBothChannelsMsg;
-        private Message fullDDSResetMsg;
+        private Message setTwoLevel;
+        private Message setSingleTone;
+        private Message selectBothChannels;
+        private Message fullDDSReset;
         
         
         [SetUp]
@@ -31,21 +31,21 @@ namespace DDSControl
 
             // Set to Two Level Modulation
             // Call to Function Register 1 according to Christian's implementation
-            setTwoLevelMsg = new Message(new byte[] { 0x01, 0xa8, 0x00, 0x20 });
+            setTwoLevel = new Message(new byte[] { 0x01, 0xa8, 0x00, 0x20 });
 
             // Set to single tone
             // Call to channel function register with AFP select none, 
             // the middle byte to default and the LSByte to all zeros
             // as in Christians code
-            setSingleToneMsg = new Message(new byte[] { 0x03, 0x00, 0x03, 0x00 });
+            setSingleTone = new Message(new byte[] { 0x03, 0x00, 0x03, 0x00 });
 
             // Select both channels
             // Call to channel select register with both channels on and open and
             // write mode MSB serial 4 bit mode
-            selectBothChannelsMsg = new Message(new byte[] { 0x00, (byte)(0xc0 + 0x36) });
+            selectBothChannels = new Message(new byte[] { 0x00, (byte)(0xc0 + 0x36) });
 
             // FullDDSReset via EP1
-            fullDDSResetMsg = new Message(new byte[] { 0x03, 0x08, 0x0b });
+            fullDDSReset = new Message(new byte[] { 0x03, 0x08, 0x0b });
             
 
         }
@@ -55,7 +55,7 @@ namespace DDSControl
         {
             // Define call
             Message call = new Message();
-            call.Add(fullDDSResetMsg);
+            call.Add(fullDDSReset);
             
             Expect.Once.On(mockDevice).Method("SendDataToEP1").With(call.ToArray());
 
@@ -68,12 +68,12 @@ namespace DDSControl
         public void TestMasterReset()
         {
             Message fullResetCall = new Message();
-            fullResetCall.Add(fullDDSResetMsg);
+            fullResetCall.Add(fullDDSReset);
 
             Message initializeCall = new Message();
-            initializeCall.Add(selectBothChannelsMsg);
-            initializeCall.Add(setSingleToneMsg);
-            initializeCall.Add(setTwoLevelMsg);
+            initializeCall.Add(selectBothChannels);
+            initializeCall.Add(setSingleTone);
+            initializeCall.Add(setTwoLevel);
 
             using (mocks.Ordered)
             {
@@ -92,8 +92,8 @@ namespace DDSControl
 
             // Define what message you expect
             Message call = new Message();
-            call.Add(selectBothChannelsMsg);
-            call.Add(setSingleToneMsg);
+            call.Add(selectBothChannels);
+            call.Add(setSingleTone);
 
             using (mocks.Ordered)
             {
@@ -108,7 +108,7 @@ namespace DDSControl
         [Test]
         public void TestSetTwoLevels()
         {
-            Expect.Once.On(mockDevice).Method("SendDataToEP2").With(setTwoLevelMsg.ToArray());
+            Expect.Once.On(mockDevice).Method("SendDataToEP2").With(setTwoLevel.ToArray());
             dds.SetLevels(2);
             mocks.VerifyAllExpectationsHaveBeenMet();
             
@@ -125,13 +125,13 @@ namespace DDSControl
         private double fout;
         private byte[] CFTWCall;
 
-        private Message fullDDSResetMsg;
-        private Message setTwoLevelMsg;
-        private Message setSingleToneMsg;
-        private Message selectBothChannelsMsg;
-        private Message selectChannelZeroMsg;
-        private Message selectChannelOneMsg;
-        private Message setFreqTo100MHzMsg;
+        private Message fullDDSReset;
+        private Message setTwoLevel;
+        private Message setSingleTone;
+        private Message selectBothChannels;
+        private Message selectChannelZero;
+        private Message selectChannelOne;
+        private Message setFreqTo100MHz;
         private Message setPhaseToZero;
         private Message initialization;
 
@@ -168,41 +168,41 @@ namespace DDSControl
 
             // Define some messages
             // FullDDSReset via EP1
-            fullDDSResetMsg = new Message(new byte[] { 0x03, 0x08, 0x0b });
+            fullDDSReset = new Message(new byte[] { 0x03, 0x08, 0x0b });
 
             // Set to Two Level Modulation
             // Call to Function Register 1 according to Christian's implementation
-            setTwoLevelMsg = new Message(new byte[] { 0x01, 0xa8, 0x00, 0x20 });
+            setTwoLevel = new Message(new byte[] { 0x01, 0xa8, 0x00, 0x20 });
 
             // Set to single tone
             // Call to channel function register with AFP select none, 
             // the middle byte to default and the LSByte to all zeros
             // as in Christians code
-            setSingleToneMsg = new Message(new byte[] { 0x03, 0x00, 0x03, 0x00 });
+            setSingleTone = new Message(new byte[] { 0x03, 0x00, 0x03, 0x00 });
 
             // Select both channels
             // Call to channel select register with both channels on and open and
             // write mode MSB serial 4 bit mode
-            selectBothChannelsMsg = new Message(new byte[] { 0x00, (byte)(0xc0 + 0x36) });
+            selectBothChannels = new Message(new byte[] { 0x00, (byte)(0xc0 + 0x36) });
 
             // Select channel one
-            selectChannelZeroMsg = new Message(new byte[] { 0x00, 0x76 });
+            selectChannelZero = new Message(new byte[] { 0x00, 0x76 });
             
             // Select channel two
-            selectChannelOneMsg = new Message(new byte[] { 0x00, 0xB6 });
+            selectChannelOne = new Message(new byte[] { 0x00, 0xB6 });
 
             // Set frequency to 100 MHz
             // 0x33 0x33 0x33 0x33 / 2**32 = 0.2
-            setFreqTo100MHzMsg = new Message(new byte[] { 0x04, 0x33, 0x33, 0x33, 0x33 });
+            setFreqTo100MHz = new Message(new byte[] { 0x04, 0x33, 0x33, 0x33, 0x33 });
 
             // Set phase to zero
             setPhaseToZero = new Message(new byte[] { 0x05, 0x00, 0x00 });
             
             // Initialization after MasterReset
             initialization = new Message();
-            initialization.Add(selectBothChannelsMsg);
-            initialization.Add(setSingleToneMsg);
-            initialization.Add(setTwoLevelMsg);
+            initialization.Add(selectBothChannels);
+            initialization.Add(setSingleTone);
+            initialization.Add(setTwoLevel);
         }
         [Test]
         public void TestSetFrequencyTo100Mhz()
@@ -237,19 +237,19 @@ namespace DDSControl
             // there is an additional phase shift of pi on the board
             Message setChannelsCall = new Message();
 
-            setChannelsCall.Add(setTwoLevelMsg); // Set modulation levels to two
-            setChannelsCall.Add(selectBothChannelsMsg); // Select both channels
-            setChannelsCall.Add(setSingleToneMsg); // Set singletone mode
-            setChannelsCall.Add(selectChannelZeroMsg); // Select channel 0
-            setChannelsCall.Add(setFreqTo100MHzMsg); // Set freq to 100MHz
+            setChannelsCall.Add(setTwoLevel); // Set modulation levels to two
+            setChannelsCall.Add(selectBothChannels); // Select both channels
+            setChannelsCall.Add(setSingleTone); // Set singletone mode
+            setChannelsCall.Add(selectChannelZero); // Select channel 0
+            setChannelsCall.Add(setFreqTo100MHz); // Set freq to 100MHz
             setChannelsCall.Add(setPhaseToZero); // Set phase to zero (always as reference)
-            setChannelsCall.Add(selectChannelOneMsg); // Select channel 1
-            setChannelsCall.Add(setFreqTo100MHzMsg); // Set freq to 100 Mhz
+            setChannelsCall.Add(selectChannelOne); // Select channel 1
+            setChannelsCall.Add(setFreqTo100MHz); // Set freq to 100 Mhz
             setChannelsCall.Add(setPhaseToZero); // Set phase to zero (since there is a phase shift of pi on the pcb)
 
             using (mocks.Ordered)
             {
-                Expect.Once.On(mockDevice).Method("SendDataToEP1").With(fullDDSResetMsg.ToArray());
+                Expect.Once.On(mockDevice).Method("SendDataToEP1").With(fullDDSReset.ToArray());
                 Expect.Once.On(mockDevice).Method("SendDataToEP2").With(initialization.ToArray());
                 Expect.Once.On(mockDevice).Method("SendDataToEP2").With(setChannelsCall.ToArray());
             }
@@ -278,7 +278,7 @@ namespace DDSControl
 
             using (mocks.Ordered)
             {
-                Expect.Once.On(mockDevice).Method("SendDataToEP1").With(fullDDSResetMsg.ToArray());
+                Expect.Once.On(mockDevice).Method("SendDataToEP1").With(fullDDSReset.ToArray());
                 Expect.Once.On(mockDevice).Method("SendDataToEP2").With(initialization.ToArray());
                 Expect.Once.On(mockDevice).Method("SendDataToEP2").With(call.ToArray());
             }
