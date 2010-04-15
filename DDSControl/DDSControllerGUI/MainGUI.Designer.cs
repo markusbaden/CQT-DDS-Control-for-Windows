0119ef3af68bb2b87fc49170b81b9d056cce0d95
Markus <markus@localhost>
2010-04-15 13:58:06 +0000

    Renamed DebugKnopf

--- DDSControl/DDSControllerGUI/MainGUI.Designer.cs
+++ DDSControl/DDSControllerGUI/MainGUI.Designer.cs
@@ -199,11 +199,11 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(73, 154);
+            this.button2.Location = new System.Drawing.Point(56, 443);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(131, 23);
             this.button2.TabIndex = 14;
-            this.button2.Text = "button2";
+            this.button2.Text = "Der Debugknopf";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
